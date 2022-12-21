1aaef1b7e8c0a23cf9a2d22a5f0e37aa1d76acdb
cshsgy <sihe_chen@u.nus.edu>
2022-12-20 18:29:07 -0800

    added cubed sphere hpp

--- src/bvals/cubed_sphere.hpp
+++ src/bvals/cubed_sphere.hpp
@@ -4,4 +4,7 @@
 void TransformOxForCubedSphere(int *ox1, int *ox2, int *tox1, int *tox2,
   LogicalLocation const& loc);
 
+void PackDataCubedSphere(const AthenaArray<Real> &src, Real *buf,
+         int sn, int en, int si, int ei, int sj, int ej, int sk, int ek, int &offset,
+         int ox2, int ox3,LogicalLocation const& loc);
 #endif
