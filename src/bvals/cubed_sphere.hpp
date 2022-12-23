faa38563031a69f3b80caa4da6d0e742306626f8
cshsgy <sihe_chen@u.nus.edu>
2022-12-22 20:37:06 -0800

    Added Cubed Sphere coordinate headers, changed the coordinate calculations

--- src/bvals/cubed_sphere.hpp
+++ src/bvals/cubed_sphere.hpp
@@ -7,5 +7,9 @@ void TransformOxForCubedSphere(int *ox2, int *ox3, int *tox2, int *tox3,
 void PackDataCubedSphere(const AthenaArray<Real> &src, Real *buf,
          int sn, int en, int si, int ei, int sj, int ej, int sk, int ek, int &offset,
          int ox1, int ox2, int ox3,LogicalLocation const& loc);
+
+Real CubedSphereMeshGeneratorX2(Real x, LogicalLocation const& loc);
+Real CubedSphereMeshGeneratorX3(Real x, LogicalLocation const& loc);
+
 #endif
 
