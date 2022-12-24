7c7f2653af50a7038897df4c7a9bf9564df996ed
cshsgy <sihe_chen@u.nus.edu>
2022-12-23 17:16:26 -0800

    Flux communication

--- src/cubed_sphere.hpp
+++ src/cubed_sphere.hpp
@@ -20,5 +20,20 @@ class GnomonicEquiangle : public Coordinates {
  public:
   GnomonicEquiangle(MeshBlock *pmb, ParameterInput *pin, bool flag);
 };
+
+class CubedSphereLR{
+  public:
+    void SetMeshBlock(MeshBlock *pmb_in);
+    void InitializeSizes(int nc3, int nc2, int nc1);
+    void SaveLR3DValues(AthenaArray<Real> &L_in, AthenaArray<Real> &R_in, int direction, int k, int j, int il, int iu);
+    void LoadLR3DValues(AthenaArray<Real> &L_in, AthenaArray<Real> &R_in, int direction, int k, int j, int il, int iu);
+    void SynchronizeFluxes();
+    void SendNeighborBlocks(LogicalLocation const& loc, int ox2, int ox3, int tg_rank, int tg_gid);
+    void RecvNeighborBlocks(LogicalLocation const& loc, int ox2, int ox3, int tg_rank, int tg_gid);
+
+    AthenaArray<Real> L3DValues, R3DValues;
+    MeshBlock *pmb;
+};
+
 #endif
 
