8c561db467f6f036733122e796c5569d907257e0
chengcli <chengcli@umich.edu>
2023-06-30 16:27:35 -0400

    add more outputs

---
+++ bench/dummy.cpp
@@ -0,0 +1,17 @@
+// athena
+#include <athena/outputs/user_outputs.hpp>
+
+void DebugOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag)
+{}
+
+void NetcdfOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag)
+{}
+
+void NetcdfOutput::CombineBlocks()
+{}
+
+void PnetcdfOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag)
+{}
+
+void FITSOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag)
+{}
